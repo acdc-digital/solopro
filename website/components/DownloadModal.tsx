d60f61b67a47b477e0f4741eae1f74687a33c6c7
Matthew C. Simon <127530566+acdc-digital@users.noreply.github.com>
2025-06-21 19:40:10 -0300

    Updated Release Version v1.6.2

--- website/components/DownloadModal.tsx
+++ website/components/DownloadModal.tsx
@@ -28,21 +28,21 @@ export function DownloadModal({ children }: DownloadModalProps) {
       icon: <Monitor className="w-6 h-6" />,
       description: 'Windows 10 or later (64-bit)',
       buttonText: 'Download for Windows',
-      url: 'https://github.com/acdc-digital/solopro/releases/download/v1.6.1/Soloist.Pro-Setup-1.6.1.exe',
+      url: 'https://github.com/acdc-digital/solopro/releases/download/v1.6.2/Soloist.Pro-Setup-1.6.2.exe',
     },
     {
       os: 'macOS',
       icon: <Apple className="w-6 h-6" />,
       description: 'macOS 10.15 or later',
       buttonText: 'Download for Mac',
-      url: 'https://github.com/acdc-digital/solopro/releases/download/v1.6.1/Soloist.Pro-1.6.1-x64.dmg',
+      url: 'https://github.com/acdc-digital/solopro/releases/download/v1.6.2/Soloist.Pro-1.6.2-x64.dmg',
     },
     {
       os: 'Linux',
       icon: <Package className="w-6 h-6" />,
       description: 'AppImage for most distributions',
       buttonText: 'Download for Linux',
-      url: 'https://github.com/acdc-digital/solopro/releases/download/v1.6.1/Soloist.Pro-1.6.1.AppImage',
+      url: 'https://github.com/acdc-digital/solopro/releases/download/v1.6.2/Soloist.Pro-1.6.2.AppImage',
     },
   ];
 
